0e93d4fd4a8cc2bb681833bb2401da489d8ec884
AP24110010337 <adityakashyap2727@gmail.com>
2026-03-28 18:05:30 +0530

    Update Pico3D.tsx

--- src/components/Pico3D.tsx
+++ src/components/Pico3D.tsx
@@ -375,7 +375,7 @@ export default function Pico3D() {
   return (
     <div style={{ width: "100%", height: "100%", minHeight: 0 }}>
       <Canvas
-        camera={{ position: [0, 0.5, 3.2], fov: 55 }}
+        camera={{ position: [0, 0.45, 3.8], fov: 60 }}
         gl={{ antialias: true, alpha: true }}
         style={{ background: "transparent", width: "100%", height: "100%" }}
       >
